ef258fb0efb93699b40309f569d9130c1b13a16e
noam meir <noamariel8@gmail.com>
2022-11-08 01:24:40 +0200

    better style and noted to next time

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,27 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Carousel } from 'antd';
 import './Home.css'
+import { Ad } from '../Ad';
 
 const contentStyle = {
   // background: 'url("../imgs/ad1.webp")',
   // backgroundSize: 'cover',
-  width: '100%'
+  width: '100%',
 };
 
 const Home = () => {
+  const [ads,setAds] = useState([])
+  useEffect(
+    ()=>{
+      setAds([{
+        src: 'https://www.gancube.com/wp-content/uploads/2021/09/Lark20210924-212043.jpg'
+      },{
+        src: 'https://www.gancube.com/wp-content/uploads/2021/09/Lark20210924-212043.jpg'
+      },{
+        src: 'https://www.gancube.com/wp-content/uploads/2021/09/Lark20210924-212043.jpg'
+      }])
+    }
+  ,[])
+  // onClick: direct to product page
   return (
     <div>
       <Carousel autoplay>
-        <div>
-          <img src='../imgs/ad1.jpg' style={contentStyle}></img>
-        </div>
-        <div>
-          <img src='../imgs/ad1.jpg' style={contentStyle}></img>
-        </div>
-        <div>
-          <img src='../imgs/ad1.jpg' style={contentStyle}></img>
-        </div>
+        {
+          ads.map(ad => {return <Ad src={ad.src} style={contentStyle}></Ad>})
+        }
       </Carousel>
+      <div className='popular-products'>
+        <div className='popular-title'>
+          <span>Popular's</span>
+        </div>
+        <div className='products-container'>
+          {/* {make products Component} */}
+        </div>
+      </div>
     </div>
   )
 }
